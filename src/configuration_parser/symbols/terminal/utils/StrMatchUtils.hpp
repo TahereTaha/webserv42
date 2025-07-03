e33d2da01e40702a04c69529a2c65fe83362c832
Carolina Papes <capapes@car14s6.42barcelona.com>
2025-07-03 15:08:16 +0200

    HTPP keywords WIP

--- src/configuration_parser/symbols/terminal/utils/StrMatchUtils.hpp
+++ src/configuration_parser/symbols/terminal/utils/StrMatchUtils.hpp
@@ -6,7 +6,7 @@
 /*   By: capapes <capapes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 12:23:20 by capapes           #+#    #+#             */
-/*   Updated: 2025/07/03 12:35:34 by capapes          ###   ########.fr       */
+/*   Updated: 2025/07/03 14:36:15 by capapes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <string>
 #include <iostream>
 
-std::string getFirstOcurrence(const std::string& str, const std::string& toFind, bool isCaseSensitive = true);
+bool 		startsWith(const std::string& str, const std::string& toFind, bool caseSensitive = true);
 std::string strIter(std::string str, int (*func)(int));
 std::string toUpper(std::string str);
 std::string toLower(std::string str);
