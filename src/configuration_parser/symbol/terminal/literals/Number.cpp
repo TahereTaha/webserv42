a5d5268c21ba30a77534c9b82e24cfadb2d2ac20
Roberta Giambona <giambonaroberta0@gmail.com>
2025-07-03 13:53:58 +0200

    Update Number.cpp

--- src/configuration_parser/symbol/terminal/literals/Number.cpp
+++ src/configuration_parser/symbol/terminal/literals/Number.cpp
@@ -24,3 +24,15 @@ Symbol* Number::clone() const
 	return new Number(*this);
 }
 
+size_t	Number::getTerminalSizeOnStr(const std::string & str)
+{
+	size_t					size = 0;
+	std::string::iterator	it = str.begin();
+
+	while (it != str.end() && isdigit(*it))
+	{
+		size++;
+		it++;
+	}
+	return (size);
+}
