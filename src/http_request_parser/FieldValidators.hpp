5035bc999a5404d511226ab13e2ee5366d569b13
Carolina Papes <capapes@cbr10s2.42barcelona.com>
2025-07-17 15:20:40 +0200

    http parser

--- src/http_request_parser/FieldValidators.hpp
+++ src/http_request_parser/FieldValidators.hpp
@@ -1,31 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   AKeyword.hpp                                       :+:      :+:    :+:   */
+/*   FieldValidators.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: capapes <capapes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/03 12:40:41 by capapes           #+#    #+#             */
-/*   Updated: 2025/07/03 15:00:42 by capapes          ###   ########.fr       */
+/*   Created: 2025/07/17 15:15:06 by capapes           #+#    #+#             */
+/*   Updated: 2025/07/17 15:16:09 by capapes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #pragma once
 
-#include <ATerminal.hpp>
-#include <StrMatchUtils.hpp>
+#include <stdexcept>
+#include <string>
 
-class AKeyword : public ATerminal
-{
-	private:
-		AKeyword();
-	protected:
-		std::string	_keyword;
-	public:
-		AKeyword(const std::string & src);
-		~AKeyword(void);
-
-		virtual size_t		getTerminalSizeOnStr(const std::string & str) const override; // careful, override is C++11
-		virtual ATerminal* 	clone(void) const = 0;
-};
+void isValidFieldName(const std::string& fieldName);
+void isValidfieldValue(const std::string& fieldValue);
+void isValidMethod(const std::string& method, int &statusCode);
+void isValidProtocolVersion(const std::string& version, int &statusCode);
+void isValidRequestTarget(const std::string& target, int &statusCode);
