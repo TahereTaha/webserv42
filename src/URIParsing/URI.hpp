855677757f6c52bd6efb52fb4067a253b3cc60c0
Taha Tahere <elberkani.taha@gmail.com>
2025-10-22 12:01:05 +0200

    adding andeling for relative references to the URI parser.

--- src/URIParsing/URI.hpp
+++ src/URIParsing/URI.hpp
@@ -11,11 +11,23 @@
 //	this uri is made with the sole purpose of handeling http uris.
 //	using this class with other kinds of uri will result in undefined behevor.
 
+typedef enum
+{
+	FULL_FORM = 0,
+	NETWORK_PATH,
+	ABSOLUTE_PATH,
+	RELATIVE_PATH,
+}	t_reference_type;
+
 class URI
 {
 	private:
 		std::string	_text;	//	the uri in a string format.
 		std::vector<std::string>	_tokens;
+
+		t_reference_type	_type;
+
+		void	identifyURIType(void);
 
 		Scheme		_scheme;
 //		Auchority	_authority;
