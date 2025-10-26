ebda389525f8d9f5441882913d9dfa4824d9143e
Taha Tahere <elberkani.taha@gmail.com>
2025-10-26 19:03:50 +0100

    added the identify type to the ip literal.

--- src/URIParsing/IpLiteral.cpp
+++ src/URIParsing/IpLiteral.cpp
@@ -1,16 +1,36 @@
 #include <IpLiteral.hpp>
 
+#include <stdexcept>
+
 IpLiteral::IpLiteral(void)
 {
-	
 }
 
 IpLiteral::IpLiteral(std::string text)
 {
 	this->_text = text;
+	this->identify_type();
+	if (this->_type == IP_V_FUTURE)
+		throw (std::invalid_argument("ip literal unsuported"));
 }
 
 IpLiteral::~IpLiteral(void)
 {
 }
+
+void	IpLiteral::identify_type(void)
+{
+	this->_type = IP_V_4;
+	if (this->_text.size < 3)
+		return ;
+	if (this->_text[0] == '[')
+		this->_type = IP_V_6;
+	if (this->_text[0] == '[' && this->_text[1] == 'v')
+		this->_type = IP_V_FUTURE;
+	if (this->_text[0] != '[')
+		this->_type = IP_V_4;
+}
+
+//	127.0.0.1
+//	[223:234::233:24]
 
--- src/URIParsing/IpLiteral.hpp
+++ src/URIParsing/IpLiteral.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
 #include <string>
+#include <stdint.h>
+
+#define IP_MAX_DATA_SIZE 16
+
+typedef enum
+{
+	IP_V_4;
+	IP_V_6;
+	IP_V_FUTURE;
+}	t_IpLiteral_type;
 
 class IpLiteral
 {
 	private:
 		std::string	_text;
+
+		t_IpLiteral_type	_type;
+
+		uint8_t	data[IP_MAX_DATA_SIZE];
 	protected:
 	public:
 		IpLiteral(void);
