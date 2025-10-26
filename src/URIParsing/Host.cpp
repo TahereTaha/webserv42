901316ee56f86f179a6e3e6f70a6edbe665c9cd9
Taha Tahere <elberkani.taha@gmail.com>
2025-10-26 11:00:40 +0100

    identifing the host type.

--- src/URIParsing/Host.cpp
+++ src/URIParsing/Host.cpp
@@ -1,7 +1,12 @@
 #include <Host.hpp>
 
+#include <stdexcept>
 #include <vector>
 #include <string>
+#include <cctype>
+#include <limits>
+
+#include <tokenize.hpp>
 
 //	helper functions.
 
@@ -20,6 +25,8 @@ static std::string	consume_IP_LITERAL(std::vector<std::string>::iterator &iter, 
 		text += *iter;
 		iter++;
 	}
+	if (*iter != "]")
+		throw (std::invalid_argument("incorrect host"));
 	if (iter != end)
 	{
 		text += *iter;
@@ -38,6 +45,74 @@ static std::string	consume_REG_NAME(std::vector<std::string>::iterator &iter, \
 		iter++;
 	}
 	return (text);
+}
+
+#define digit_char	"0123456789abcdefghijklmnopqrstuvwxyz"
+
+static int	stoi(std::string num_str, size_t *pos = 0, int base = 10)
+{
+	size_t	i = 0;
+	long	num = 0;
+	int		digit = 0;
+	int		sign = 1;
+
+	//	skip all the spaces.
+	while (i < num_str.size() && isspace(num_str[i]))
+		i++;
+	if (i < num_str.size() && num_str[i] == '+')
+		i++;
+	else if (i < num_str.size() && num_str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	if (i == num_str.size() || !isalnum(num_str[i]))
+		throw (std::invalid_argument("not a int"));
+	while (i < num_str.size() && isalnum(num_str[i]))
+	{
+		digit = std::string(digit_char).find(num_str[i]);
+		digit *= sign;
+		num *= base;
+		num += digit;
+		if (num > std::numeric_limits<int>::max())
+			throw (std::out_of_range("more than max int"));
+		if (num < std::numeric_limits<int>::min())
+			throw (std::out_of_range("less than min int"));
+		i++;
+	}
+	if (pos != NULL)
+		*pos = i;
+	return (num);
+}
+
+static int	is_IPV4address(std::string text)
+{
+	size_t	i = 0;
+	int		octet;
+	size_t	octet_count= 0;
+	while (i < text.size() && octet_count < 4)
+	{
+		try
+		{
+			octet = stoi(text.substr(i), &i);
+		}
+		catch (...)
+		{
+			return (0);
+		}
+		if (octet < 0 || octet > 255)
+			return (0);
+		if (i < text.size())
+		{
+			if (text[i] != '.')
+				return (0);
+			i++;
+		}
+		octet_count++;
+	}
+	if (i + 1 != text.size())
+		return (0);
+	return (1);
 }
 
 //	class methods.
@@ -70,5 +145,20 @@ Host::~Host(void)
 
 void	Host::identifyHostType(void)
 {
+	if (this->_text[0] == '[')
+	{
+		this->_type = IP_LITERAL;
+		if (this->_text[1] == 'v')
+			this->_type = IP_LITERAL_V_FUTURE;
+		else
+			this->_type = IP_LITERAL_V6_ADDRESS;
+		if (this->_type == IP_LITERAL_V_FUTURE)
+			this->_type = IP_LITERAL_UNSUPORTED;	//	this will be changad to suport more versions
+		return ;
+	}
+	if (is_IPV4address(this->_text))
+		this->_type = IP_V4_ADDRESS;
+	else
+		this->_type = REG_NAME;
 }
 
