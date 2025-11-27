ad258c7797064533fe0713215a55fbd48af1b29e
Taha Tahere <elberkani.taha@gmail.com>
2025-11-27 20:35:36 +0100

    adding the evaluate method to the error pages

--- src/configuration_parser/symbol/non_terminal/SymbolErrorPage.hpp
+++ src/configuration_parser/symbol/non_terminal/SymbolErrorPage.hpp
@@ -5,9 +5,12 @@
 #include <ATerminal.hpp>
 #include <ANonTerminal.hpp>
 
+#include <intermediate_representation.hpp>
+
 class SymbolErrorPage : public ANonTerminal
 {
 	private:
+		std::vector<t_error_page>	_error_pages;
 	protected:
 	public:
 		SymbolErrorPage(void);
@@ -18,4 +21,5 @@ class SymbolErrorPage : public ANonTerminal
 		void			evaluate(Tree<AEvaluable*> *self);
 
 		AParser	*getParser(void) const;
+		std::vector<t_error_page>	getErrorPages(void) const ;
 };
