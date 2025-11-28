56239a7f4e75d31ae159044fcc05c6365f310868
Taha Tahere <elberkani.taha@gmail.com>
2025-11-28 15:44:38 +0100

    made the evaluation for the server symbol

--- src/configuration_parser/symbol/non_terminal/SymbolServer.hpp
+++ src/configuration_parser/symbol/non_terminal/SymbolServer.hpp
@@ -5,9 +5,35 @@
 #include <ATerminal.hpp>
 #include <ANonTerminal.hpp>
 
+#include <intermediate_representation.hpp>
+
+#include <SymbolServerName.hpp>
+#include <SymbolListen.hpp>
+#include <SymbolClientMaxBodySize.hpp>
+#include <SymbolErrorPage.hpp>
+#include <SymbolLocation.hpp>
+
+typedef enum
+{
+	SERVER_NAME,
+	SERVER_LISTEN,
+	SERVER_CLIENT_MAX_BODY_SIZE,
+	SERVER_ERROR_PAGE,
+	SERVER_LOCATION,
+}	t_server_directives;
+
 class SymbolServer : public ANonTerminal
 {
 	private:
+		t_server	_server;
+
+		int			set_up_directive(AEvaluable *directive);
+		
+		int			set_up_directive_name(SymbolServerName *directive);
+		int			set_up_directive_listen(SymbolListen *directive);
+		int			set_up_directive_client_max_body_size(SymbolClientMaxBodySize *directive);
+		int			set_up_directive_error_page(SymbolErrorPage *directive);
+		int			set_up_directive_location(SymbolLocation *directive);
 	protected:
 	public:
 		SymbolServer(void);
@@ -18,4 +44,5 @@ class SymbolServer : public ANonTerminal
 		void			evaluate(Tree<AEvaluable*> *self);
 
 		AParser	*getParser(void) const;
+		t_server	getServer(void) const ;
 };
