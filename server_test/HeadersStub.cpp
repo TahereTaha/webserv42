7f4274dd177450f12a7201635fab58bc622ab561
Roberta Giambona <nemo@Robertas-MacBook-Pro.local>
2025-11-28 11:51:05 +0100

    refactor of Server and ServerResponse, addedd comments for peer review and test environment for Server testing in isolation

---
+++ server_test/HeadersStub.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+bool isValidKey(const std::string &key) {
+	(void)key;       
+	return true;     
+}
+
+
+bool isValidValue(const std::string &value) {
+	(void)value;     
+	return true;    
