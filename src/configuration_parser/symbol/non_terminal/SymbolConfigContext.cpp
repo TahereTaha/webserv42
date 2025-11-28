e7670227781f570cc51c6e03430cd613d3f4a621
Taha Tahere <elberkani.taha@gmail.com>
2025-11-28 16:35:40 +0100

    adding the evaluate to the config context

--- src/configuration_parser/symbol/non_terminal/SymbolConfigContext.cpp
+++ src/configuration_parser/symbol/non_terminal/SymbolConfigContext.cpp
@@ -30,9 +30,23 @@ SymbolConfigContext	*SymbolConfigContext::clone(void) const
 	return (new SymbolConfigContext(*this));
 }
 
+#include <IR_printing_functions.hpp>
+
 void		SymbolConfigContext::evaluate(Tree<AEvaluable*> *self)
 {
-	(void) self;
+	size_t	i = 0;
+	while (i < self->getChildNodeSize())
+	{
+		this->_servers.push_back(dynamic_cast<SymbolServer *>(self->getChildNode(i)->getContent())->getServer());
+		i++;
+	}
+
+	i = 0;
+	while (i < this->_servers.size())
+	{
+		printServer(this->_servers[i]);
+		i++;
+	}
 }
 
 AParser	*SymbolConfigContext::getParser(void) const
@@ -40,5 +54,10 @@ AParser	*SymbolConfigContext::getParser(void) const
 	AParsingRule	*rule = new ParsingRuleRepetition(1, -1, \
 			new ParsingRuleSymbol(SymbolServer().clone()));
 	return (new AParser(this->clone(), rule));
+}
+
+std::vector<t_server>	SymbolConfigContext::getServers(void) const 
+{
+	return (this->_servers);
 }
 
